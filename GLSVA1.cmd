b9fec4d44b7b9a6719b2c59cdc8c0fdf047bc29a
Timothy Everhart <formalbearbaron@gmail.com>
2015-04-06 11:50:43 -0700

    Update and rename GLSVJ2.cmd to GLSVA1.cmd
    Input Box Working!

--- GLSVA1.cmd
+++ GLSVA1.cmd
@@ -1,8 +1,16 @@
 @echo off
+set GLS=
 :wathel
 echo Hello
+set GLS= Hello
+msg * Hello
+echo.
 :chat
-set /p environment=C:
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set environment=%%G
+DEL %TEMP%\~input.vbs
 if /i "%environment%" == "Jasper" goto foryou
 if /i "%environment%" == "Hello" goto wathel
 if /i "%environment%"== "Good Bye" goto sure
@@ -112,29 +120,141 @@ if /i "%environment%"== "Sky." goto recog
 if /i "%environment%"== "Formal." goto recog
 if /i "%environment%"== "Bear." goto recog
 if /i "%environment%"== "Admin." goto recog
+echo.
 echo May I request you rephrase that?
+set GLS= May I request you rephrase that?
+msg * May I request you rephrase that?
+echo.
 goto chat
+:achat
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set environment=%%G
+DEL %TEMP%\~input.vbs
+if /i "%environment%" == "close" goto endcl
+if /i "%environment%" == "close?" goto acheck
+if /i "%environment%" == "close!" goto endcl
+if /i "%environment%" == "close." goto endcl
+if /i "%environment%" == "chat?" goto acheck
+if /i "%environment%"== "chat" goto chat
+if /i "%environment%"== "chat." goto chat
+if /i "%environment%"== "chat" goto chat
+if /i "%environment%"== "chat!" goto chat
+if /i "%environment%"== "edit?" goto acheck
+if /i "%environment%"== "edit!" goto edit
+if /i "%environment%"== "edit." goto edit
+if /i "%environment%"== "edit" goto edit
+if /i "%environment%"== "Jasper" goto foryou
+echo.
+echo That is not a command.
+set GLS= That is not a command.
+msg * That is not a command.
+echo.
+goto achat
+:acheck
+echo.
+echo If this is the command you actually wish to run, please retype the command without a (?).
+set GLS= If this is the command you actually wish to run, please retype the command without a (?).
+msg * If this is the command you actually wish to run, please retype the command without a (?).
+echo.
+goto achat
+:edit
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set environment=%%G
+DEL %TEMP%\~input.vbs
+if /i "%environment%" == "find" goto find
+if /i "%environment%" == "exit" goto achat
+echo.
+echo That is not a command.
+set GLS= That is not a command.
+msg * That is not a command.
+echo.
+goto edit
+:find
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set INPUT=%%G
+DEL %TEMP%\~input.vbs
+echo.
+echo What is the desired change?
+set GLS= What is the desired change?
+msg * What is the desired change?
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set ANPUT=%%G
+DEL %TEMP%\~input.vbs
+echo.
+echo.
+TYPE GLSVA1.txt
+echo.
+echo.
+goto edit
+:endcl
+exit
 :intro
+echo.
 echo I am GLS, or Game Library System. I can find any specified game for you and provide you with information about that game.
+set GLS= I am GLS, or Game Library System. I can find any specified game for you and provide you with information about that game.
+msg * I am GLS, or Game Library System. I can find any specified game for you and provide you with information about that game.
+echo.
 echo However, Who are you?
+set GLS= However, Who are you?
+msg * However, Who are you? 
+echo.
 goto chat
 :its
+echo.
 echo Nice to meet you.
+set GLS= Nice to meet you.
+msg * Nice to meet you.
+echo.
 goto chat
 :recog
+echo.
 echo Hello Admin.
-goto chat
+set GLS= Hello Admin.
+msg * Hello Admin.
+echo.
+goto achat
 :techin
+echo.
 echo I am a Game Library System, or GLS.
+set GLS= I am a Game Library System, or GLS.
+msg * I am a Game Library System, or GLS.
+echo.
 echo I can provide a list of all known games created, and historical and general information about all of these games.
+set GLS= I can provide a list of all known games created, and historical and general information about all of these games.
+msg * I can provide a list of all known games created, and historical and general information about all of these games.
+echo.
 goto chat
 :ok
+echo.
 echo Ok.
+set GLS= ok.
+msg * ok.
+echo.
 goto chat
 :sure
+echo.
 echo Do you actually intend to leave?
+set GLS= Do you actually intend to leave?
+msg * Do you actually intend to leave?
 :check
-set /p environment=C:
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set environment=%%G
+DEL %TEMP%\~input.vbs
 if /i "%environment%" == "No" goto ok
 if /i "%environment%" == "Yes" goto end
 if /i "%environment%" == "No!" goto ok
@@ -143,13 +263,26 @@ if /i "%environment%" == "No." goto ok
 if /i "%environment%" == "Yes." goto end
 if /i "%environment%" == "N" goto ok
 if /i "%environment%" == "Y" goto end
+echo.
 echo Please use from this list: Y, N, Yes, No, Yes!, No!, Yes., No.
+set GLS= Please use from this list: Y, N, Yes, No, Yes!, No!, Yes., No.
+msg * Please use from this list: Y, N, Yes, No, Yes!, No!, Yes., No.
+echo.
 goto check
 :quest
+echo.
 echo Is this your real identity, please answer with honest. Nothing will happen to the computer by answering either way it just provides a good test.
+set GLS= Is this your real identity, please answer with honest. Nothing will happen to the computer by answering either way it just provides a good test.
+msg * Is this your real identity, please answer with honest. Nothing will happen to the computer by answering either way it just provides a good test.
+echo.
 goto chat
 :foryou
-set /p environment=J:
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set environment=%%G
+DEL %TEMP%\~input.vbs
 if /i "%environment%" == "fuck you" goto fno
 if /i "%environment%" == "fuck you." goto fno
 if /i "%environment%" == "fuck you!" goto fno
@@ -157,17 +290,37 @@ if /i "%environment%" == "fuck" goto fstp
 if /i "%environment%" == "fuck." goto fstp
 if /i "%environment%" == "fuck!" goto fstp
 if /i "%environment%" == "Chat" goto chat
+echo.
 echo Jasper we both know what you want to say.
+set GLS= Jasper we both know what you want to say.
+msg * Jasper we both know what you want to say.
+echo.
 goto foryou
 :fno
+echo.
 echo No, fuck you my dear friend.
+set GLS= No, fuck you my dear friend.
+msg * No, fuck you my dear friend.
+echo.
 goto foryou
 :fstp
+echo.
 echo This chat may be for you, but please refrain from indirectness in my brain.
+set GLS= This chat may be for you, but please refrain from indirectness in my brain.
+msg * This chat may be for you, but please refrain from indirectness in my brain.
+echo.
 goto foryou
 :end
+echo.
 echo This will shut down your computer unless you close the window. Are you still sure?
-set /p environment=C:
+set GLS= This will shut down your computer unless you close the window. Are you still sure?
+msg * This will shut down your computer unless you close the window. Are you still sure?
+echo.
+set _prompt=%1
+ECHO Wscript.Echo Inputbox("GLS Input %_prompt%","GLS")>%TEMP%\~input.vbs
+:s_GetInput
+FOR /f "delims=/" %%G IN ('cscript //nologo %TEMP%\~input.vbs') DO set environment=%%G
+DEL %TEMP%\~input.vbs
 if /i "%environment%" == "No" goto ok
 if /i "%environment%" == "Yes" goto end
 if /i "%environment%" == "No!" goto ok
@@ -176,7 +329,17 @@ if /i "%environment%" == "No." goto ok
 if /i "%environment%" == "Yes." goto end
 if /i "%environment%" == "N" goto ok
 if /i "%environment%" == "Y" goto end
+echo.
+echo Please give a direct and simple answer.
+set GLS= Please give a direct and simple answer.
+msg * Please give a direct and simple answer.
+echo.
+goto end
 :bye
+echo.
 echo Good Bye.
+set GLS= Good Bye.
+msg * Good Bye.
+echo.
 Pause
 shutdown/l
